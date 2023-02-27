b00a95f499a160dc1c172acd63cd98fa04580557
rupnaz <yonexrobban@gmail.com>
2023-02-27 14:27:31 +0100

    added jsx and css files

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import "./Nav.css"
 
 const Nav = () => {
     return (
-        <div>
-            
+        <div className='nav'>
+            <h2>Meny</h2>
+            <h2>Vårt kaffe</h2>
+            <h2>Min profil</h2>
+            <h2>Orderstatus</h2>
+            <h2>Orderhistorik</h2>
         </div>
     );
 };
