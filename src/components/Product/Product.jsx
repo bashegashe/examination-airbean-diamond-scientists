c1b68f7bfa2229969e43572395de52e34f7dbada
NguyenRobin <robin.nnguyen@hotmail.com>
2023-02-28 11:21:12 +0100

    added dots to each product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Product.css';
-import plusSign from '../../assets/Vector 3.svg';
+import plusSignLogo from '../../assets/Vector 3.svg';
 function Product() {
   return (
     <article className="product">
       <section className="product__add">
-        <img src={plusSign} alt="" />
+        <img src={plusSignLogo} alt="" />
       </section>
       <section className="product__name">
-        <h3>Bryggkaffe..........</h3>
+        <h3>
+          Bryggkaffe<span>...............</span>
+        </h3>
         <p>Bryggd på månadens bönor</p>
       </section>
       <h3>49 kr</h3>
