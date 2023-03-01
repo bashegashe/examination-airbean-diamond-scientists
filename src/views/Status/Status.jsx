09a2ef351fc3ca58da1f2d2c06a7a7043d611a8f
Bachir Sbinati <sbinatibachir@gmail.com>
2023-03-01 17:40:49 +0100

    Add props to status view

--- src/views/Status/Status.jsx
+++ src/views/Status/Status.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 import Button from '../../components/Button/Button';
 import drone from '../../assets/Group 5.svg'
 import styles from "./Status.module.css"
+import { useNavigate, Link } from 'react-router-dom';
 
-const Status = () => {
+const Status = (props) => {
+    const navigate = useNavigate();
+
     return (
         <div className={styles.status}>
-            <header className={styles.status__header}><p>Ordernummer <span>#12DV23F</span></p></header>
+            <header className={styles.status__header}><p>Ordernummer <span>{props.orderNr}</span></p></header>
             <section className={styles['status__section-drone']}>
                 <img src={drone}/>           
             </section>
             <section className={styles['status__section-mid']}>
                 <h2>Din beställning är på väg!</h2>
-                <p><span>13</span> minuter</p>
+                <p><span>{props.eta}</span> minuter</p>
             </section>
             <section className={styles['status__section-bot']}>
-                <Button type={'light'}>Ok, cool!</Button>
+                <Link to="/about"><Button type={'light'}>Ok, cool!</Button></Link>
             </section>
         </div>
     );
