7afd0575ba69192764b3e00ec13a07f4f5af5044
Bachir Sbinati <sbinatibachir@gmail.com>
2023-03-01 14:24:49 +0100

    Add state to Cart.jsx component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,7 +3,13 @@ import styles from './Cart.module.css';
 import CartItem from './CartItem/CartItem';
 import Button from '../Button/Button';
 
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+
 function Cart(props) {
+    const [cartTotal, setCartTotal] = useState(0);
+    const cart = useSelector((state) => state.cart);
+
     return (
         <section className={styles.cart}>
             <div className={styles.cart__arrow}></div>
@@ -11,13 +17,26 @@ function Cart(props) {
             <div className={styles.cart__container}>
                 <h2>Din beställning</h2>
 
-                <CartItem title="Bryggkaffe" quantity="1" price="98 kr" />
+                {
+                    cart.map((cartItem) => {
+                        setCartTotal((total) => total + cartItem.quantity * cartItem.price);
+
+                        return (
+                            <CartItem
+                                title={cartItem.title}
+                                quantity={cartItem.quantity}
+                                price={cartItem.price + ' kr'}
+                                key={cartItem.id}
+                            />
+                        )
+                    })
+                }
 
                 <section className={styles.cartTotal}>
                     <article className={styles.cartTotal__main}>
                         <h4 className={styles.cartTotal__title}>Total</h4>
                         <span className={styles.cartTotal__separator}></span>
-                        <h4 className={styles.cartTotal__price}>98 kr</h4>
+                        <h4 className={styles.cartTotal__price}>{cartTotal + ' kr'}</h4>
                     </article>
 
                     <span className={styles.cartTotal__info}>inkl. moms + drönarleverans</span>
