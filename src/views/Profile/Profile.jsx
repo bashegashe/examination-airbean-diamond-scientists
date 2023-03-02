0553d68e00c1e7c9ad2a10eed4d9d78f0eb0cef6
Bachir Sbinati <sbinatibachir@gmail.com>
2023-03-02 10:48:26 +0100

    Make profile navbutton redirect to nav

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -4,6 +4,7 @@ import styles from './Profile.module.css'
 import headerPicture from '../../assets/header.svg'
 import menuIcon from '../../assets/navicon-open.svg'
 import * as api from '../../utils/api';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
     const [loggedIn, setLoggedIn] = useState(false);
@@ -27,7 +28,7 @@ const Profile = () => {
     return (
         <div className={styles.profile}>
             <header>
-                <img className={styles['profile__img-icon']} src={menuIcon} />
+                <Link to="/nav"><img className={styles['profile__img-icon']} src={menuIcon} /></Link>
                 <img src={headerPicture} />
             </header>
             <ProfileForm setLoggedIn={setLoggedIn} />
